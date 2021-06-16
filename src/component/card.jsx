6cc9be350e2518e32a701d3649ab7a16e2b7a89f
Gilang Novrizal <gilnov.201196@gmail.com>
2021-06-16 12:09:45 +0700

    add image and button

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -6,22 +6,62 @@ const useStyles = createUseStyles({
     marginTop: "2rem",
     fontFamily: "Poppins, sans-serif",
   },
-  cardTitle:{
-      marginBottom: "0.5rem",
-      fontWeight: 500,
-      fontSize: "48px"
+  cardTitle: {
+    marginBottom: "0.5rem",
+    fontWeight: 500,
+    fontSize: "48px",
   },
-  cardYear:{
-      marginBottom: "1rem",
-      fontWeight: 500,
-      color: "#777777",
-      fontSize: "14px"
+  cardYear: {
+    marginBottom: "1rem",
+    fontWeight: 500,
+    color: "#777777",
+    fontSize: "14px",
   },
-  cardDescription:{
-      color: "#777777",
-      fontWeight: 300,
-      fontSize: "18px"
-  }
+  cardDescription: {
+    color: "#777777",
+    fontWeight: 300,
+    fontSize: "18px",
+  },
+  imageContainer: {
+    width: "100%",
+    marginBottom: "1rem",
+  },
+  image: {
+    width: "100%",
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "10px 20px",
+    border: ".25px solid",
+    borderColor: "rgb(67 90 111 / 30%)",
+    borderRadius: "5px",
+    background: "#F9F9FB",
+    color: "#425A70",
+    fontWeight: 500,
+    fontSize: "16px",
+    fontFamily: "Poppins, sans-serif",
+    cursor: "pointer",
+    outline: "none",
+    boxShadow: "inset 0 1px 0 0 rgb(255 255 255 / 40%)",
+    marginTop: "1rem",
+    "&:hover": {
+      transition: "all .25s",
+      background: "#EDEFF1",
+    },
+
+    "&:disabled": {
+      backgroundColor: "#AFAFAF",
+      cursor: "not-allowed",
+      color: "#F3F3F3",
+      border: "#AFAFAF",
+    },
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+  },
 });
 
 const WorkCard = (props) => {
@@ -30,10 +70,15 @@ const WorkCard = (props) => {
     <div className={classes.card}>
       <h1 className={classes.cardTitle}>{props.title}</h1>
       <h3 className={classes.cardYear}>{props.year}</h3>
+      <div className={classes.imageContainer}>
+        <img src={props.image} alt={props.imageAlt} className={classes.image} />
+      </div>
       <p className={classes.cardDescription}>{props.description}</p>
-      <a href={props.link}>
-        <button>Explore More</button>
-      </a>
+      <div className={classes.buttonContainer}>
+        <a href={props.link} target="_blank">
+          <button className={classes.button}>Explore More</button>
+        </a>
+      </div>
     </div>
   );
 };
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
 import profile from "../public/profile.jpg";
+import etera from "../public/etera.png";
+import sinarmas from "../public/sinarmas.png";
+import kotakode_event from "../public/kotakode_event.png";
+import kotakode_story from "../public/kotakode_story.png";
 import WorkCard from "../component/card";
 
 const works = [
   {
     name: "Kotakode User Journey",
     year: 2021,
+    image: kotakode_story,
     description: `So many programmers want to build a resume but sometimes they got confused about which is the best 
       format for their resume. With Kotakode User Journey feature now Kotakode users can easily create their resume 
       in the best format. Users can show their highlights, tech stack, and experiences on their resume. The resume is 
       very useful for job searching and another purpose. This resume format is recognized by the tech recruiter. I was 
       involved in developing the front end of this feature.`,
+    link: "https://kotakode.com/users/4543/gilnov?activeTab=story",
   },
   {
     name: "Kotakode Event",
     year: 2021,
+    image: kotakode_event,
     description: `Kotakode often held talk show events like KotaTalks and some other events. If you want to access the event 
     you have to fill the form on the google form, and access the other link if you have a question that you want to ask.
     This feature simplifies user access to the event of kotakode.com. Users can register for the upcoming kotakode event and 
@@ -23,21 +30,26 @@ const works = [
     that other users ask for the event and also vote the question you might like or dislike. Users can also watch the past event 
     video that has been uploaded. All that can be done only with access the kotakode.com/events
     `,
+    link: "https://kotakode.com/events",
   },
   {
     name: "events.sinarmasland.com ",
     year: 2021,
+    image: sinarmas,
     description: `events.sinarmasland.com is a website for access Sinarmas Land online events. I was involved in developing some features
      of the website and some bug fixing on the existing website. I was handling upload attendees feature for the report, events list page, 
      and email reminder for the event's participant. I also optimized the website performance by refactoring the existing code. The tech 
      stacks of this website are React JS, Express JS, and MySQL database.   `,
+    link: "https://events.sinarmasland.com/",
   },
   {
     name: "Eteratrade.com",
     year: 2020,
+    image: etera,
     description: `Eteratrade.com was my first professional website project. This website was created for importer company office needs and 
     company profile. Mostly I worked on the front end side, but also worked on the back end side for developing the API. This website was built
      with React JS for the front end, Express JS for the backend, and MySQL database. `,
+    link: "https://eteratrade.com/",
   },
 ];
 
@@ -62,7 +74,7 @@ const useStyles = createUseStyles({
     marginTop: "2rem",
     borderRadius: "25px",
     padding: "2rem",
-    marginBottom: "2rem"
+    marginBottom: "2rem",
   },
   profileContainer: {
     width: "100%",
@@ -158,6 +170,9 @@ const LandingPage = () => {
                 title={work.name}
                 year={work.year}
                 description={work.description}
+                image={work.image}
+                imageAlt={work.name}
+                link={work.link}
               />
             );
           })}
